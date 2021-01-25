27fa8fc6526725d9b1c41be7394c0957b67051e9
Andrew Lee <andrewleebcs@gmail.com>
2021-01-25 09:47:56 +0900

    1000

---
+++ BOJ/1000-1099/A+B.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const* argv[]) {
+
+    int A;
+    int B;
+
+    cin >> A;
+    cin >> B;
+
+    cout << A + B;
+    return 0;
+}
+
+// 20.01.15 Andrew Lee (dltjrgh)
