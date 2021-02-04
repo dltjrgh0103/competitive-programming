0bed5b2f2f4f97cd0312ce5966c423207e8bc07d
Andrew Lee <andrewleebcs@gmail.com>
2021-02-04 21:46:58 +0900

    1028

---
+++ BOJ/1000-1099/다이아몬드 광산.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+ll arr[1508][1508];
+ll ld[808][808];
+ll rd[808][808];
+ll lu[808][808];
+ll ru[808][808];
+
+int main() {
+    ll i, j, k, l, m, n;
+    scanf("%lld %lld", &n, &m);
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= m; j++)
+            scanf("%1lld", &arr[i][j]);
+
+    for (i = n; i >= 1; i--)
+        for (j = 1; j <= m; j++)
+            if (arr[i][j] == 1) {
+                ld[i][j] = ld[i + 1][j - 1] + 1;
+                rd[i][j] = rd[i + 1][j + 1] + 1;
+            }
+
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= m; j++)
+            if (arr[i][j] == 1) {
+                lu[i][j] = lu[i - 1][j - 1] + 1;
+                ru[i][j] = ru[i - 1][j + 1] + 1;
+            }
+
+    ll ans = 0;
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= m; j++) {
+            for (k = 1; k <= min(ld[i][j], rd[i][j]); k++)
+                if (arr[i + 2 * (k - 1)][j] && lu[i + 2 * (k - 1)][j] >= k && ru[i + 2 * (k - 1)][j] >= k)
+                    ans = max(ans, k);
+            for (k = 1; k <= min(ru[i][j], rd[i][j]); k++)
+                if (arr[i][j + 2 * (k - 1)] && lu[i][j + 2 * (k - 1)] >= k && ld[i][j + 2 * (k - 1)] >= k)
+                    ans = max(ans, k);
+
+        }
+    printf("%lld", ans);
+}
