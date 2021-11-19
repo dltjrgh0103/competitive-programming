e1a1107a057d4f855f71d9e55ea6efbcff695f56
Andrew Lee <andrewleebcs@gmail.com>
2021-11-19 19:09:51 +0900

    4344

---
+++ BOJ/4300-4399/평균은 넘겠지.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+void func();
+
+int main(int argc, const char* argv[]) {
+
+	int C;
+	cin >> C; // test case
+
+	for (int i = 0; i < C; i++) {
+		func();
+	}
+
+	return 0;
+}
+
+void func() {
+	int N;
+	cin >> N;
+
+	// 동적 배열 선언 및 생성
+	int* arr = new int[N]; // 혹은 int* arr = (int*)malloc(sizeOf(int) * size)
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i]; // i ~ N - 1 까지 입력받은 요소로 초기화
+	}
+
+	// 평균 구하기
+	double avg = 0;
+	for (int i = 0; i < N; i++) {
+		avg += arr[i]; // 모든 성적 누적합
+	}
+	avg = avg / N; // 누적합에 대해 N으로 나눈다.
+
+	// 평균 점수를 넘는 인원 수 구하기
+	double count = 0;
+	for (int i = 0; i < N; i++) {
+		if (arr[i] > avg) {
+			count++;
+		}
+	}
+
+	// 평균을 넘는 인원 %
+	double result = (count / N) * 100;
+	cout << fixed;
+	cout.precision(3);
+	cout << result << "%\n";
+
+	delete[] arr; // 동적 할당을 할 경우에는 더이상 안쓰는 경우 반드시 메모리 해제를 해야한다.
+}
+
+// 20.01.17 Andrew Lee (dltjrgh)
